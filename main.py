93a4799e4031af7e4c5fef2ba9f6c95f4e6954ce
Levetator <ilevetator@yandex.ru>
2021-02-05 17:26:06 +0300

    first

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('')
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class Coffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.load_table()
+
+    def load_table(self):
+        with sqlite3.connect('coffee.sqlite') as con:
+            res = con.cursor().execute("SELECT * FROM coffee").fetchall()
+        self.table.setColumnCount(7)
+        self.table.setRowCount(0)
+        self.table.setHorizontalHeaderLabels(('ID', 'Сорт', 'Cтепень обжарки',
+                                              'Молотый', 'Описание вкуса', 'Цена', 'Объём'))
+        for i, row in enumerate(res):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.table.resizeColumnsToContents()
+        for i in range(self.table.rowCount()):
+            for j in range(self.table.columnCount()):
+                self.table.item(i, j).setFlags(self.table.item(i, j).flags() ^ Qt.ItemIsEditable)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Coffee()
+    ex.show()
+    sys.exit(app.exec())
